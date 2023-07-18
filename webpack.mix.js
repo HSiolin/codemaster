2dcb9e96d003e96ee2a74ed837139427040fc164
Hillary77 <hillaryssima876@gmail.com>
2023-07-18 14:41:13 -0300

    Template jogo

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,15 +6,17 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel applications. By default, we are compiling the CSS  
  | file for the application as well as bundling up all the JS files.
  |
  */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]) 
+// js('resources/js/app.js', 'public/js')
+ //.postCss('resources/css/app.css', 'public/css', [
+     //
+ //]) 
+mix.scripts('node_modules/jquery/dist/jquery.js','public/site/js/jquery.js')
+.scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js','public/site/js/bootstrap.bundle.js')
+.sass('node_modules/bootstrap/scss/bootstrap.scss', 'public/site/css/bootstrap.css')
     
     .version()
     .sourceMaps();
